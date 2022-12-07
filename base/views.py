3b497d8c96612bcfec4f2a196973e27f1763d0bb
Jabs <98640151+JT142@users.noreply.github.com>
2022-12-07 17:29:13 +0800

    Added rooms dictionary

--- base/views.py
+++ base/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id':1, 'name':'Django support group'},
+    {'id':2, 'name':'Debugging Django'},
+    {'id':3, 'name':'React study group'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'home.html',context)
 
 def room(request):
     return render(request, 'room.html')

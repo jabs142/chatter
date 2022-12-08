bde0767cddcb090f20b7e71686f78f28d1182cc5
Jabs <98640151+JT142@users.noreply.github.com>
2022-12-08 14:25:56 +0800

    Created model for room

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    # host =
+    # topic= 
+    name = models.CharField(max_Length =200) # specify the attribute and its value
+    description = models.TextField(null = True, blank = True) # by default null is set to False. Null = true means description can be blank. Null is for database and blank is for the form
+    # participants = 
+    updated = models.DateTimeField(auto_now = True) #snapshot every time we save
+    created = models.DateTimeField(auto_now_add = True) #snapshot the first time we add it
+
+    def __str__(self):
+        return self.name
+

7d367ecc31920cb7487ffc0f2bf83dbe2f2df2f4
Jabs <98640151+JT142@users.noreply.github.com>
2022-12-13 11:15:10 +0800

    Added pillows for models.ImageField

--- chatter/settings.py
+++ chatter/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+
+AUTH_USER_MODEL = 'base.User'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
